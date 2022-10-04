de3660b0fa41b6fa16f384dd30899340f0682e03
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-04 20:16:29 +0100

    Added - connection to snowflake

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,3 +7,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+USER = os.environ.get('USER')
+ACCOUNT = os.environ.get('ACCOUNT')
+PASSWORD = os.environ.get('PASSWORD')
+WAREHOUSE= os.environ.get('WAREHOUSE')
+DATABASE= os.environ.get('DATABASE')
+SCHEMA= os.environ.get('SCHEMA')
+
+conn = snowflake.connector.connect(
+    user=USER,
+    password=PASSWORD,
+    account=ACCOUNT,
+    warehouse=WAREHOUSE,
+    database=DATABASE,
+    schema=SCHEMA
+)
+cs = conn.cursor()
