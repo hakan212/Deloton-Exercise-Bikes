83df7437c5e3913a7f36627d0f9e06696beda108
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-04 20:36:47 +0100

    Removed - snowflake conn and added to backend

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,38 +1,12 @@
-import dash
 import dash_bootstrap_components as dbc
 from dash import Dash, html
-import snowflake.connector
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-# TODO: decide on the schema to read from in snowflake (data mart)
+app = Dash(__name__, use_pages=False)
 
-# Snowflake Connection
-
-USER = os.environ.get('USER')
-ACCOUNT = os.environ.get('ACCOUNT')
-PASSWORD = os.environ.get('PASSWORD')
-WAREHOUSE= os.environ.get('WAREHOUSE')
-DATABASE= os.environ.get('DATABASE')
-SCHEMA= os.environ.get('SCHEMA')
-
-conn = snowflake.connector.connect(
-    user=USER,
-    password=PASSWORD,
-    account=ACCOUNT,
-    warehouse=WAREHOUSE,
-    database=DATABASE,
-    schema=SCHEMA
+app.layout = html.Div(
+    html.H1("Delaton Dashboard")
 )
-cs = conn.cursor()
-
-
-# Dash application
-
-app = Dash(__name__, use_pages=True)
-
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", debug=True, port=8080)
