245df7d85dbd854011eeec66fbe1dcac570356a0
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-05 14:07:40 +0100

    Added - function to take SQL queries

--- restful_api/app.py
+++ restful_api/app.py
@@ -1,56 +1,71 @@
-import pandas as pd 
 import json
-import sqlalchemy
-import snowflake.connector
-from snowflake.sqlalchemy import URL
-from sqlalchemy import create_engine
-from flask import Flask, request
-from dotenv import load_dotenv
 import os
+
+import pandas as pd
+import snowflake.connector as sf
+from dotenv import load_dotenv
+from flask import Flask, request
 
 load_dotenv()
 
-USER = os.environ.get('USER')
-ACCOUNT = os.environ.get('ACCOUNT')
-PASSWORD = os.environ.get('PASSWORD')
-WAREHOUSE= os.environ.get('WAREHOUSE')
-DATABASE= os.environ.get('DATABASE')
-SCHEMA= os.environ.get('SCHEMA')
+USER = os.environ.get("USER")
+ACCOUNT = os.environ.get("ACCOUNT")
+PASSWORD = os.environ.get("PASSWORD")
+WAREHOUSE = os.environ.get("WAREHOUSE")
+DATABASE = os.environ.get("DATABASE")
+SCHEMA = os.environ.get("SCHEMA")
 
 flask_app = Flask(__name__)
 
-# SQLalchemy connection
-
-engine = create_engine(URL(
-    user=USER,
-    password=PASSWORD,
-    account=ACCOUNT,
-    warehouse=WAREHOUSE,
-    database=DATABASE,
-    schema=SCHEMA
-))
-
-connection = engine.connect()
-
-
 # Snowflake connection
 
-conn = snowflake.connector.connect(
+conn = sf.connect(
     user=USER,
     password=PASSWORD,
     account=ACCOUNT,
     warehouse=WAREHOUSE,
     database=DATABASE,
-    schema=SCHEMA
+    schema=SCHEMA,
 )
-cs = conn.cursor()
+
+def run_query(conn, query):
+    """Runs a SQL query in Snowflake data warehouse
+
+    Args:
+        conn: Snowflake DB connection
+        query: SQL query
+
+    Returns:
+        query_results: data associated with the query made
+    """
+    cs = conn.cursor()
+    cs.execute(query)
+    query_results = cs.fetchall()
+    cs.close()
+    return query_results
 
 
 # API Endpoints
 
+# GET Endpoints
 @flask_app.route('/')
-def index():
+def default():
     return "Delaton Exercise Bikes API"
+
+@flask_app.route("/rider/<rider_id>", method=["GET"])
+def get_rider_id(rider_id);
+    snow_sql = request.args.get
+
+@flask_app.route("/rider/<rider_id>/rides", method=["GET"])
+
+
+@flask_app.route("/daily", method=["GET"])
+#daily endpoint should handle both daily and daily + query string
+
+
+# DELETE Endpoints
+@flask_app.route("/ride/<ride_id>", method=["DELETE"])
+def delete_ride_id(ride_id):
 
 
 if __name__ == "__main__":
