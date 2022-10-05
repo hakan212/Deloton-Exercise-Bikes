f6f81819afd0534f87040073edeb27e8e3dc0b49
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-05 12:12:18 +0100

    Added - snowflake connection

--- restful_api/app.py
+++ restful_api/app.py
@@ -1,7 +1,38 @@
 import pandas as pd 
 import json
 import sqlalchemy
-import snowflake.connector as sf
+import snowflake.connector
 from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# SQLalchemy connection
+
+engine = create_engine(URL(
+
+))
+
+
+
+# Snowflake connection
+
+USER = os.environ.get('USER')
+ACCOUNT = os.environ.get('ACCOUNT')
+PASSWORD = os.environ.get('PASSWORD')
+WAREHOUSE= os.environ.get('WAREHOUSE')
+DATABASE= os.environ.get('DATABASE')
+SCHEMA= os.environ.get('SCHEMA')
+
+conn = snowflake.connector.connect(
+    user=USER,
+    password=PASSWORD,
+    account=ACCOUNT,
+    warehouse=WAREHOUSE,
+    database=DATABASE,
+    schema=SCHEMA
+)
+cs = conn.cursor()
