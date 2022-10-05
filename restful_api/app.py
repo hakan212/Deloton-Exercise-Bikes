8259622a25759600279c02bdeab06ea5bd25fb34
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-05 21:11:53 +0100

    Updated - changed to snowflake conn + fixed query

--- restful_api/app.py
+++ restful_api/app.py
@@ -1,10 +1,11 @@
 import os
 from datetime import datetime, timedelta
+import json
+import snowflake.connector as sf
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
-from snowflake.sqlalchemy import URL
-from sqlalchemy import create_engine
+
 
 load_dotenv()
 
@@ -13,14 +14,21 @@ ACCOUNT = os.environ.get("ACCOUNT")
 PASSWORD = os.environ.get("PASSWORD")
 WAREHOUSE = os.environ.get("WAREHOUSE")
 DATABASE = os.environ.get("DATABASE")
-SCHEMA = os.environ.get("SCHEMA")  # need to change to the correct table
+BATCH_SCHEMA = os.environ.get("BATCH_SCHEMA") # now the name of the batch production schema
 
 flask_app = Flask(__name__)
 
 # Snowflake-SQLalchemy connection
+conn = sf.connect(
+        user="admin",
+        password=PASSWORD,
+        account=ACCOUNT,
+        warehouse=WAREHOUSE,
+        database=DATABASE,
+        schema=BATCH_SCHEMA
+)
 
-
-def run_query(query):
+def run_query(query, conn):
     """Runs a SQL query in Snowflake data warehouse
 
     Args:
@@ -30,30 +38,11 @@ def run_query(query):
     Returns:
         query_results: data associated with the query made
     """
-    engine = create_engine(
-        URL(
-            user=USER,
-            password=PASSWORD,
-            account=ACCOUNT,
-            warehouse=WAREHOUSE,
-            database=DATABASE,
-            schema=SCHEMA,
-        )
-    )
+    cursor = conn.cursor()
 
-    conn = engine.connect()
-
-    try:
-        query_results = conn.execute(query)
-    except Exception as e:
-        print(f"Exception: {e}")
-    finally:
-        conn.close()
-        engine.dispose()
-
-    data = query_results.fetchall()
-
-    return data
+    query_results = cursor.execute(query)
+   
+    return query_results
 
 
 # API Endpoints
@@ -63,98 +52,115 @@ def run_query(query):
 def default():
     return "Delaton Exercise Bikes API"
 
-
-@flask_app.route("/ride/<ride_id>", method=["GET"])
-def get_ride(ride_id):
+@flask_app.route("/rides", methods=["GET"])
+def get_rides():
     query = f"""
         SELECT *
             FROM rides
-            WHERE id = {ride_id}
+        LIMIT 10
     """
-    ride = run_query(query)
+    query_results = run_query(query, conn)
 
-    response = jsonify({"status": 200, "ride": ride})
+    json_string = query_results.fetch_pandas_all().to_json(orient="records")
+
+    parsed_json = json.loads(json_string)
+
+    response = jsonify({"status": 200, "rides": parsed_json})
 
     return response
 
 
-@flask_app.route("/rider/<rider_id>", method=["GET"])
-def get_rider(rider_id):
-    query = f"""
-        SELECT *
-            FROM riders
-            WHERE id = {rider_id}
-    """
-    rider = run_query(query)
+# @flask_app.route("/ride/<ride_id>", methods=["GET"])
+# def get_ride(ride_id):
+#     query = f"""
+#         SELECT *
+#             FROM rides
+#             WHERE ride_id = {ride_id}
+#     """
+#     ride = run_query(query)
 
-    response = jsonify({"status": 200, "rider": rider})
+#     response = jsonify({"status": 200, "ride": ride})
 
-    return response
-
-
-@flask_app.route("/rider/<rider_id>/rides", method=["GET"])
-def get_rides_for_rider(rider_id):
-    query = f"""
-        SELECT *
-            FROM rides
-            WHERE rider_id = {rider_id}
-    """
-    rides = run_query(query)
-
-    response = jsonify({"status": 200, "rides": rides})
-
-    return response
+#     return response
 
 
-@flask_app.route("/daily", method=["GET"])
-# daily endpoint should handle both daily and daily + query string
-def get_daily():
-    requested_date = request.args.get("date")
+# @flask_app.route("/rider/<rider_id>", methods=["GET"])
+# def get_rider(rider_id):
+#     query = f"""
+#         SELECT *
+#             FROM users
+#             WHERE user_id = {rider_id}
+#     """
+#     rider = run_query(query)
 
-    if requested_date is not None:
-        query = f"""
-            SELECT *
-                FROM rides
-                WHERE begin_timestamp = {requested_date}
-        """
-        requested_date_rides = run_query(query)
+#     response = jsonify({"status": 200, "rider": rider})
 
-        response = jsonify({"status": 200, "rides": requested_date_rides})
-
-        return response
-
-    query = f"""
-        SELECT *
-            FROM rides
-            WHERE begin_timestamp > {datetime.now() - timedelta(days=1)}
-        """
-    daily_rides = run_query(query)
-
-    response = jsonify({"status": 200, "rides": daily_rides})
-
-    return response
+#     return response
 
 
-# DELETE Endpoints
-@flask_app.route("/ride/<ride_id>", method=["POST"])
-# Seems like DELETE requests aren't supported - workaround with POST for now
-def delete_ride_id(ride_id):
-    query = f"""
-        DELETE FROM rides
-            WHERE id = {ride_id}
-    """
-    run_query(query)
+# @flask_app.route("/rider/<rider_id>/rides", methods=["GET"])
+# def get_rides_for_rider(rider_id):
+#     query = f"""
+#         SELECT *
+#             FROM rides
+#             WHERE rider_id = {rider_id}
+#     """
+#     rides = run_query(query)
 
-    second_query = f"""
-        SELECT *
-            FROM rides
-            WHERE id = {ride_id}
-    """
-    ride = run_query(second_query)
+#     response = jsonify({"status": 200, "rides": rides})
 
-    response = jsonify({"status": 204, "ride": ride})
+#     return response
 
-    return response
+
+# @flask_app.route("/daily", methods=["GET"])
+# # daily endpoint should handle both daily and daily + query string
+# def get_daily():
+#     requested_date = request.args.get("date")
+
+#     if requested_date is not None:
+#         query = f"""
+#             SELECT *
+#                 FROM rides
+#                 WHERE begin_timestamp = {requested_date}
+#         """
+#         requested_date_rides = run_query(query)
+
+#         response = jsonify({"status": 200, "rides": requested_date_rides})
+
+#         return response
+
+#     query = f"""
+#         SELECT *
+#             FROM rides
+#             WHERE begin_timestamp > {datetime.now() - timedelta(days=1)}
+#         """
+#     daily_rides = run_query(query)
+
+#     response = jsonify({"status": 200, "rides": daily_rides})
+
+#     return response
+
+
+# # DELETE Endpoints
+# @flask_app.route("/ride/<ride_id>", methods=["POST"])
+# # Seems like DELETE requests aren't supported - workaround with POST for now
+# def delete_ride_id(ride_id):
+#     query = f"""
+#         DELETE FROM rides
+#             WHERE id = {ride_id}
+#     """
+#     run_query(query)
+
+#     second_query = f"""
+#         SELECT *
+#             FROM rides
+#             WHERE id = {ride_id}
+#     """
+#     ride = run_query(second_query)
+
+#     response = jsonify({"status": 204, "ride": ride})
+
+#     return response
 
 
 if __name__ == "__main__":
