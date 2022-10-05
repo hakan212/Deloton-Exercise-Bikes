82db6b3671eb1cc85b582bba9564baea26f6562d
noahryan98 <trainee.noah.ryan@sigmalabs.co.uk>
2022-10-05 16:01:04 +0100

    Actioned comments

--- heart_rate_calculation/heart_rate_calculator.py
+++ heart_rate_calculation/heart_rate_calculator.py
@@ -1,16 +1,13 @@
-def calculate_max_heart_rate (age: int):
-## Calculates maximum heart rate given age
+def calculate_max_heart_rate (age: int) -> int:
+## Calculates maximum heart rate given age. 211, 208, 0.64 and 0.7 taken from formulas found in published papers.
     if age <=40:
         return 211 - (0.64 * age)
     else:
         return 208 - (0.7 * age)
 
-def check_if_heart_rate_abnormal (current_heart_rate: int, max_heart_rate:int):
+def check_if_heart_rate_abnormal (current_heart_rate: int, max_heart_rate:int) -> bool:
 ## Returns true if current heart rate is abnormal
     upper_limit = 0.9 * max_heart_rate
     lower_limit = 0.5 * max_heart_rate
 
-    if lower_limit <= current_heart_rate <= max_heart_rate:
-        return False
-    else:
-        return True
+    return lower_limit <= current_heart_rate <= upper_limit
