4eb78abb4496cbfd7adc6581481abc4a6049ecd6
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-05 15:29:04 +0100

    Added - endpoints for daily and delete (POST)

--- restful_api/app.py
+++ restful_api/app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 import pandas as pd
 import sqlalchemy
@@ -7,7 +8,9 @@ import snowflake.connector as sf
 from sqlalchemy import create_engine
 from snowflake.sqlalchemy import URL
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -16,20 +19,22 @@ ACCOUNT = os.environ.get("ACCOUNT")
 PASSWORD = os.environ.get("PASSWORD")
 WAREHOUSE = os.environ.get("WAREHOUSE")
 DATABASE = os.environ.get("DATABASE")
-SCHEMA = os.environ.get("SCHEMA") # need to change to the correct table
+SCHEMA = os.environ.get("SCHEMA")  # need to change to the correct table
 
 flask_app = Flask(__name__)
 
 # Snowflake-SQLalchemy connection
 
-engine = create_engine(URL(
-    user=USER,
-    password=PASSWORD,
-    account=ACCOUNT,
-    warehouse=WAREHOUSE,
-    database=DATABASE,
-    schema=SCHEMA
-))
+engine = create_engine(
+    URL(
+        user=USER,
+        password=PASSWORD,
+        account=ACCOUNT,
+        warehouse=WAREHOUSE,
+        database=DATABASE,
+        schema=SCHEMA,
+    )
+)
 
 connection = engine.connect()
 
@@ -46,24 +51,25 @@ def run_query(query, engine=engine):
     """
     conn = engine.connect()
 
-    try: 
+    try:
         query_results = conn.execute(query)
     finally:
         connection.close()
- 
+
     data = query_results.fetchall()
-    
+
     return data
 
 
 # API Endpoints
 
 # GET Endpoints
-@flask_app.route('/')
+@flask_app.route("/")
 def default():
     return "Delaton Exercise Bikes API"
 
-@flask_app.route("/ride/<ride_id>")
+
+@flask_app.route("/ride/<ride_id>", method=["GET"], ["DELETE"])
 def get_ride(ride_id):
     query = f"""
         SELECT *
@@ -72,7 +78,7 @@ def get_ride(ride_id):
     """
     ride = run_query(query)
 
-    response = jsonify({"status": 200,"ride": ride})
+    response = jsonify({"status": 200, "ride": ride})
 
     return response
 
@@ -86,7 +92,7 @@ def get_rider(rider_id):
     """
     rider = run_query(query)
 
-    response = jsonify({"status": 200,"rider": rider})
+    response = jsonify({"status": 200, "rider": rider})
 
     return response
 
@@ -100,19 +106,63 @@ def get_rides_for_rider(rider_id):
     """
     rides = run_query(query)
 
-    response = jsonify({"status": 200,"rides": rides})
+    response = jsonify({"status": 200, "rides": rides})
 
     return response
 
 
 @flask_app.route("/daily", method=["GET"])
-#daily endpoint should handle both daily and daily + query string
+# daily endpoint should handle both daily and daily + query string
+def get_daily():
+    requested_date = request.args.get("date")
+
+    if requested_date is not None:
+        query = f"""
+            SELECT *
+                FROM rides
+                WHERE begin_timestamp = {requested_date}
+        """
+        requested_date_rides = run_query(query)
+
+        response = jsonify({"status": 200, "rides": requested_date_rides})
+
+        return response
+
+    else:
+
+        query = f"""
+            SELECT *
+                FROM rides
+                WHERE begin_timestamp > {datetime.now() - timedelta(days=1)}
+         """
+        daily_rides = run_query(query)
+
+        response = jsonify({"status": 200, "rides": daily_rides})
+
+        return response
 
 
 # DELETE Endpoints
-@flask_app.route("/ride/<ride_id>", method=["DELETE"])
+@flask_app.route("/ride/<ride_id>", method=["POST"])
+#s Seems like DELETE requests aren't supported - workaround with POST
 def delete_ride_id(ride_id):
+    query = f"""
+        DELETE FROM rides
+            WHERE id = {ride_id}
+    """
+    run_query(query)
 
+    second_query = f"""
+        SELECT *
+            FROM rides
+            WHERE id = {ride_id}
+    """
+    ride = run_query(second_query)
+
+    response = jsonify({"status": 204, "ride": ride})
+
+    return response
 
 if __name__ == "__main__":
+    # TODO: import waitress for when we containerize
     flask_app.run(debug=True)
