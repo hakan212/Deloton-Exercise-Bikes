70efa50a5cd83081d2f96677ceb7adf43b477970
noahryan98 <trainee.noah.ryan@sigmalabs.co.uk>
2022-10-06 16:01:41 +0100

    Tidied and added variable types & cast types

--- heart_rate_email/email_sender.py
+++ heart_rate_email/email_sender.py
@@ -2,22 +2,14 @@ import boto3
 from botocore.exceptions import ClientError
 from heart_rate_calculator import heart_rate_low, heart_rate_high
 
-SENDER = 'Deloton Exercise Co. <trainee.hakan.bas@sigmalabs.co.uk>'
-
-RECIPIENT = 'trainee.noah.ryan@sigmalabs.co.uk'
-
-AWS_REGION = 'eu-west-2'
-
-CHARSET = "UTF-8"
-
-def get_email_subject (heart_rate, age):
+def get_email_subject (heart_rate: int, age: int) -> str:
 ## Returns appropriate subject given age and heart rate
     if heart_rate_high(heart_rate, age):
         return 'WARNING: Heart rate very high.'
     if heart_rate_low(heart_rate, age):
         return 'Heart rate low during Deloton session.'
 
-def get_email_HTML_body (heart_rate, age):
+def get_email_HTML_body (heart_rate: int, age: int) -> str:
 ## Returns appropriate HTML body given age and heart rate
     if heart_rate_high(heart_rate, age):
         email_body_header = 'WARNING: Heart rate dangerously fast!'
@@ -35,7 +27,7 @@ def get_email_HTML_body (heart_rate, age):
     </html>
     '''
 
-def get_email_text_body (heart_rate, age):
+def get_email_text_body (heart_rate: int, age: int) -> str:
 ## Returns appropriate text body given age and heart rate
     if heart_rate_high(heart_rate, age):
         email_body_header = 'WARNING: Heart rate dangerously fast!'
@@ -47,6 +39,7 @@ def get_email_text_body (heart_rate, age):
     return f'''{email_body_header}\r\n
     {email_body_content}
     '''
+
 
 def send_email(aws_region: str, send_email_from:str ,recipient:str, email_body_html: str, email_body_text: str, email_subject: str ,charset: str = "UTF-8"):
 ## Sends email using AWS SES
