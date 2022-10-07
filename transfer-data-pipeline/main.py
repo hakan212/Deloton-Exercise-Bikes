8d3490052a16dbe1dd11fec8e614085799f57206
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-07 15:22:52 +0100

    Formatted - black, isort

--- transfer-data-pipeline/main.py
+++ transfer-data-pipeline/main.py
@@ -1,8 +1,7 @@
 import os
 
-import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import create_engine,inspect
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -14,18 +13,31 @@ DB_NAME = os.getenv("DB_NAME")
 MART_SCHEMA = os.getenv("MART_SCHEMA")
 PRODUCTION_SCHEMA = os.getenv("PRODUCTION_SCHEMA")
 
+
 def get_engine_connection():
-        """Connects to postgreSQL DBMS on AWS Aurora
+    """
+    Connects to postgreSQL DBMS on AWS Aurora
 
-        Returns:
-            DB engine
-        """
-        conn_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    """
+    conn_string = (
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
 
-        return create_engine(conn_string)
+    return create_engine(conn_string)
+
 
 def transfer_production_to_mart():
-     
+    """
+    Summary: queries users and rides table in the production
+    schema.
+
+    Using two CTEs to only get data that is 12 hours
+    fresh.
+
+    The two CTEs are right joined to make up the required
+    data for the recent_rides tables in the mart schema
+
+    """
     with get_engine_connection() as conn:
 
         query = f"""
@@ -55,11 +67,17 @@ def transfer_production_to_mart():
         """
 
         conn.execute(query)
-    
+
     print("transferred data from production to schema")
 
-def clear_stale_data():
 
+def clear_stale_data():
+    """
+    Executes a delete query on the recent_rides
+    table in the mart.
+
+    Data older than 12 hours ago will be deleted from the table.
+    """
     with get_engine_connection() as conn:
 
         query = f"""
@@ -68,7 +86,7 @@ def clear_stale_data():
         """
 
         conn.execute(query)
-    
+
     print("dropped rows older than 12 hours")
 
 
