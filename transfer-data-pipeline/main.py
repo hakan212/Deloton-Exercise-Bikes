da5f10e12bd244de470059908bb07dbbc19625d6
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-09 11:35:21 +0100

    Formatted - black/isort

--- transfer-data-pipeline/main.py
+++ transfer-data-pipeline/main.py
@@ -17,11 +17,21 @@ def handler(event, context):
     def get_engine_connection():
         """
         Connects to postgreSQL DBMS on AWS Aurora
-
         """
         conn_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
         return create_engine(conn_string)
+
+    def run_query(query):
+        """
+        Runs a SQL query in AWS Aurora RDBMS
+
+        """
+        engine = get_engine_connection()
+
+        query_results = engine.execute(query)
+
+        return query_results
 
     def transfer_production_to_mart():
         """
@@ -34,32 +44,31 @@ def handler(event, context):
         The two CTEs are right joined to make up the required
         data for the recent_rides tables in the mart schema
         """
-        with get_engine_connection() as conn:
 
-            query = f"""      
-                INSERT INTO {MART_SCHEMA}.recent_rides
-                
-                WITH user_gender_dob AS (
-                    SELECT user_id, gender,
-                        DATE_PART('year', AGE(CURRENT_DATE, date_of_birth))
-                            AS age 
-                        FROM {PRODUCTION_SCHEMA}.users
-                ),
-                rides_before AS (
-                    SELECT *
-                        FROM {PRODUCTION_SCHEMA}.rides
-                        WHERE begin_timestamp > (CURRENT_DATE - INTERVAL '12 hours')
-                )
-                SELECT ugd.user_id, rb.ride_id, ugd.gender, ugd.age, rb.begin_timestamp,
-                    rb.total_duration_sec, rb.total_power, rb.mean_power, rb.mean_resistance,
-                        rb.mean_rpm, rb.mean_heart_rate
+        query = f"""      
+            INSERT INTO {MART_SCHEMA}.recent_rides
+            
+            WITH user_gender_dob AS (
+                SELECT user_id, gender,
+                    DATE_PART('year', AGE(CURRENT_DATE, date_of_birth))
+                        AS age 
+                    FROM {PRODUCTION_SCHEMA}.users
+            ),
+            rides_before AS (
+                SELECT *
+                    FROM {PRODUCTION_SCHEMA}.rides
+                    WHERE begin_timestamp > (CURRENT_DATE - INTERVAL '12 hours')
+            )
+            SELECT ugd.user_id, rb.ride_id, ugd.gender, ugd.age, rb.begin_timestamp,
+                rb.total_duration_sec, rb.total_power, rb.mean_power, rb.mean_resistance,
+                    rb.mean_rpm, rb.mean_heart_rate
 
-                    FROM user_gender_dob AS ugd
-                    RIGHT JOIN rides_before AS rb
-                        ON ugd.user_id = rb.user_id
-            """
+                FROM user_gender_dob AS ugd
+                RIGHT JOIN rides_before AS rb
+                    ON ugd.user_id = rb.user_id
+        """
 
-            conn.execute(query)
+        run_query(query)
 
         print("transferred data from production to schema")
 
@@ -70,14 +79,13 @@ def handler(event, context):
 
         Data older than 12 hours ago will be deleted from the table.
         """
-        with get_engine_connection() as conn:
 
-            query = f"""
-                DELETE FROM {MART_SCHEMA}.recent_rides
-                   WHERE begin_timestamp < (CURRENT_DATE - INTERVAL '12 hours')
-            """
+        query = f"""
+            DELETE FROM {MART_SCHEMA}.recent_rides
+                WHERE begin_timestamp < (CURRENT_DATE - INTERVAL '12 hours')
+        """
 
-            conn.execute(query)
+        run_query(query)
 
         print("dropped rows older than 12 hours")
 
