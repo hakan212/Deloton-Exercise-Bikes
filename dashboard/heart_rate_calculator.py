c383b2578d93380c44c6661eba56c4f88d378778
joehasson <joe.hasson2@gmail.com>
2022-10-10 12:23:57 +0100

    Add heart_rate_ok function; format

--- dashboard/heart_rate_calculator.py
+++ dashboard/heart_rate_calculator.py
@@ -1,5 +1,5 @@
 def calculate_max_heart_rate(age: int) -> int:
-    '''Calculates maximum heart rate given age. 211, 208, 0.64 and 0.7 taken from formulas found in published papers'''
+    """Calculates maximum heart rate given age. 211, 208, 0.64 and 0.7 taken from formulas found in published papers"""
     if age <= 40:
         return 211 - (0.64 * age)
     else:
@@ -7,16 +7,27 @@ def calculate_max_heart_rate(age: int) -> int:
 
 
 def heart_rate_low(current_heart_rate: int, age: int) -> bool:
-    '''Returns true if current heart rate is abnormal'''
+    """Returns true if current heart rate is abnormal"""
     max_heart_rate = calculate_max_heart_rate(age)
     lower_limit = 0.5 * max_heart_rate
 
+    print(
+        "heart rate low", current_heart_rate <= lower_limit and current_heart_rate != 0
+    )
     return current_heart_rate <= lower_limit and current_heart_rate != 0
 
 
 def heart_rate_high(current_heart_rate: int, age: int) -> bool:
-    '''Returns true if current heart rate is abnormal'''
+    """Returns true if current heart rate is abnormal"""
     max_heart_rate = calculate_max_heart_rate(age)
     upper_limit = 0.9 * max_heart_rate
 
+    print("heart rate high", current_heart_rate >= upper_limit)
     return current_heart_rate >= upper_limit
+
+
+def heart_rate_ok(current_heart_rate: int, age: int) -> bool:
+    return not (
+        heart_rate_high(current_heart_rate, age)
+        or heart_rate_low(current_heart_rate, age)
+    )
