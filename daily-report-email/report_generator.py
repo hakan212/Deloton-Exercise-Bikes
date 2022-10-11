facbfc2c656fb25818ddbf94f0b7ccef038206c6
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-11 12:24:39 +0100

    Updated - abstracted components of pdf

--- daily-report-email/report_generator.py
+++ daily-report-email/report_generator.py
@@ -7,22 +7,24 @@ from visualisation_generator import (get_dataframe, get_mean_heart_rate,
                                      get_mean_total_power, get_number_of_rides,
                                      plot_age_rides_bar, plot_gender_rides_pie)
 
-# A4 page size in mm
-HEIGHT = 297
-WIDTH = 210
+A4_DOC_WIDTH = 210
+
+
+def create_image_header(pdf):
+    """Creates an image header with the logo"""
+    pdf.image("./assets/images/deloton-logo.png", -0.1, 0, A4_DOC_WIDTH + 1)
+    pdf.ln(5)
 
 
 def create_cover_title(pdf):
-    """
-    Creates a cover title
-    """
+    """Creates a cover title"""
     pdf.set_font("Times", "", 36)
     pdf.ln(75)
     pdf.write(5, "Deloton Daily Report")
     pdf.ln(10)
     pdf.set_font("Times", "", 20)
     pdf.write(4, f"{date.today()}")
-    pdf.ln(5)
+    pdf.ln(10)
 
 
 def create_page_title(pdf, text):
@@ -30,23 +32,16 @@ def create_page_title(pdf, text):
     pdf.set_font("Times", "", 24)
     pdf.ln(15)
     pdf.write(5, f"{text}")
-
-
-def generate_report(df_rides):
-    """Generate a PDF report with data insights"""
-    pdf = FPDF()  # A4 by default
-    pdf.compress = False
-
-    pdf.add_page()
-    pdf.image("./assets/images/deloton-logo.png", -0.1, 0, WIDTH + 1)
-    pdf.ln(5)
-
-    create_cover_title(pdf)
     pdf.line(0, 110, 210, 110)
 
-    pdf.ln(5)
-    create_page_title(pdf, "Ride Analytics")
 
+def create_text_block(pdf, df_rides):
+    """
+    Creates a text block with key ride analytics:
+    total number of rides, average total power generated,
+    average power generated per ride, and average heart rate
+    per rider.
+    """
     pdf.set_font("Times", "", 16)
     pdf.ln(15)
     pdf.write(5, f"Total number of rides: {get_number_of_rides(df_rides)}")
@@ -64,10 +59,33 @@ def generate_report(df_rides):
         5, f"Average heart rate per rider: {round(get_mean_heart_rate(df_rides))} bpm"
     )
 
+
+def create_graph_block(pdf):
+    """
+    Creates a block with two data insight graphs:
+    gender split for rides, and age split for rides
+    """
     pdf.image("./assets/tmp/gender_fig.png", 0, 185, w=95, h=100)
     pdf.image("./assets/tmp/age_fig.png", 100, 185, w=115, h=100)
 
+
+def save_pdf_file(pdf):
+    """Saves the pdf into the temp directory in assets"""
     pdf.output(name="./assets/tmp/deloton_daily_report.pdf", dest="F")
+
+
+def generate_report(df_rides):
+    """Generate a PDF report with data insights"""
+    pdf = FPDF()  # A4 by default
+    pdf.compress = False
+    pdf.add_page()
+
+    create_image_header(pdf)
+    create_cover_title(pdf)
+    create_page_title(pdf, text="Ride Analytics")
+    create_text_block(pdf, df_rides)
+    create_graph_block(pdf)
+    save_pdf_file(pdf)
 
 
 if __name__ == "__main__":
